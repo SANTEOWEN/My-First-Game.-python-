985d673167b81b5cb382c1c6ed8083a9de454855
SANTEOWEN <101460774+SANTEOWEN@users.noreply.github.com>
2023-02-18 17:25:53 +0800

    I created a introduction base on what i learn on pygame module

---
+++ intro.py
@@ -0,0 +1,42 @@
+#How videogames work
+
+#Movies > Videogames
+#Movies - Lots of images that are shown in quick succession (+sound)
+
+#video games - both just show you lots of images and your brain interprets thats as a fluid motion
+
+#movies
+#  - are fixed
+#  - No way to input(except pausing and fastforwarding)
+#game 
+# - images are dynamically generated while game is running  - position of enemies, player movement and health etc changes all the time
+# - constant checking of player input to update what frame is being drawn
+
+#CONCEPT
+
+#1. Checking player input(the event loop)
+#2. Use that information to place elements on the screen\
+#----------------------V------------------------
+#                   it creates images ________________________++
+
+#2D games > places images on the screen.
+#3D games places 3D models on the screen.
+#Both of theme can produce image(frames)
+
+#HOW PYGAME WORKS/PURPOSE
+
+#1*Pygame helps you draw images(and play sounds)
+
+#2*Check for player inputs.(input() stops your code and is thus useless for games)
+
+#3* other gamedev tolls like collisions, creating text, timer etc
+
+#----------------------------------- BAD NEWS -----------------------
+
+# Pygame is always just for the beginer 
+
+# Game engines: programmers that help you make games by giving you tools like level editors, physics engines, skeleton animations(and much more) to make games
+
+--> # making games in pygame > you solve a ton of problems yourself and become a good programmer in general > you can quickly learn a new game engine or other tools 
+
+
