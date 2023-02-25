e22123dee1092f7e4ab4c2e865428bd81cdc99ea
SANTEOWEN <101460774+SANTEOWEN@users.noreply.github.com>
2023-02-25 19:13:25 +0800

    I learn about rectangles on pygame

--- game_dev.py
+++ game_dev.py
@@ -1,32 +1,38 @@
-import pygame 
+import pygame
 from sys import exit
 
 #init() = it starts the pygame
 
 pygame.init()
 #we use pygame.display.set_mode(()) to create a screen
-#we need to create a play surface a window use to let the players see whats happening
+#we created a play surface a window use to let the players see whats happening
 screen = pygame.display.set_mode((800,400))
 pygame.display.set_caption('Runner')
-# we use the variable clock to set the default frames per seconds of a the game.
+
+# we use the variable clock to set the default frames per seconds of a the game and we use the pygame.time.Clock() to input what default ticks/frames we want
 clock = pygame.time.Clock()
 
-#we added a font to create basic text on the game.
+#we added a font to create basic text on the game. by using pygame.font.Font('filename of the font', size)
 test_font = pygame.font.Font('Fonts/Pixeltype.ttf', 50)
 # We use render() this method use to render any text surfaces and it needs some inputs whic are the ("TEXT", ANTI ALIAZING COUNT U WANT, "COLOR OF THE TEXT")
 text_surface = test_font.render('My Game', False, 'Black')
 
 #we create a surface for the game.
+
+#we have the sky_surface() variable to set the background of the game, pygame.image.load() use to load the imported images so that pygame can use it.
 sky_surface = pygame.image.load('Graphics/Sky.png').convert()
+
 ground_surface = pygame.image.load('Graphics/ground.png').convert()
 
 #Enemy Surface 
 snail_surface = pygame.image.load('Graphics/snail/snail1.png').convert_alpha()
-snail_x_pos = 600
+snail_rect = snail_surface.get_rect(midbottom = (600,300))
 
+#Player Surface/Player Rectangle
 Player_surface = pygame.image.load('Graphics/player/player_walk_1.png').convert_alpha()
 #pygame.rect() use to create a rectangle for a certain image which needs this inputs (lect,top,width,height)
-player_rectangle = pygame.rect()
+player_rectangle = Player_surface.get_rect(midbottom = (80, 300))
+
 
 #We need to create a loop that we use infinitely to open the screen window
 while True:
@@ -43,14 +49,14 @@ while True:
     screen.blit(ground_surface,(0,300))
     screen.blit(text_surface, (300,50))
 
-    #we increment the value of the X position of the snail to move it on the left side of the display
-    snail_x_pos -= 3
+    snail_rect.x -= 3
     #this condition repositions the snail if the snail hits a -100 coordinate on the display coordinate it will continue to move untile it rach again the coordinate 800 of the screen 
     #If the postion(coordinate) of the snail reaches the value of -100 under the 600 default coordinate it will continue walking till it reach the 800 value of coordiniate/position
-    if snail_x_pos < -100: snail_x_pos = 800
+    if snail_rect.right <= 0: snail_rect.left = 800
 
-    screen.blit(snail_surface, (snail_x_pos,250))
-    screen.blit(Player_surface, (80, 200))
+    #we increment the value of the X position of the snail to move it on the left side of the display
+    screen.blit(snail_surface, snail_rect)
+    screen.blit(Player_surface, player_rectangle)
     #inside this loop we draw all of our elements 
     # and update everything that insides the game
     pygame.display.update()
