0ba6773b74cdf6825ab2749f69354fadd531112f
SANTEOWEN <101460774+SANTEOWEN@users.noreply.github.com>
2023-03-01 22:08:25 +0800

    + today I learned how to draw shapes using the pygame.draw() method and also add colors on all of the shapes with hex code colors.
    + I also learned how to get keyboard inputs and use to create a movement for the character
    
    + I also learn the logic/physics of gravity on any game. (not the actual physics its more like look accurate)

--- game_dev.py
+++ game_dev.py
@@ -7,7 +7,7 @@ pygame.init()
 #we use pygame.display.set_mode(()) to create a screen
 #we created a play surface a window use to let the players see whats happening
 screen = pygame.display.set_mode((800,400))
-pygame.display.set_caption('Runner')
+pygame.display.set_caption('Takbo')
 
 # we use the variable clock to set the default frames per seconds of a the game and we use the pygame.time.Clock() to input what default ticks/frames we want
 clock = pygame.time.Clock()
@@ -15,7 +15,8 @@ clock = pygame.time.Clock()
 #we added a font to create basic text on the game. by using pygame.font.Font('filename of the font', size)
 test_font = pygame.font.Font('Fonts/Pixeltype.ttf', 50)
 # We use render() this method use to render any text surfaces and it needs some inputs whic are the ("TEXT", ANTI ALIAZING COUNT U WANT, "COLOR OF THE TEXT")
-score_surf = test_font.render('My Game', False, 'Black')
+
+score_surf = test_font.render('My Game', False, (0, 0, 0))
 score_rect = score_surf.get_rect(center = (400, 50))
 
 #we create a surface for the game.
@@ -32,10 +33,10 @@ snail_rect = snail_surf.get_rect(midbottom = (600,300))
 Player_surf = pygame.image.load('Graphics/player/player_walk_1.png').convert_alpha()
 #pygame.rect() use to create a rectangle for a certain image which needs this inputs (lect,top,width,height)
 player_rectangle = Player_surf.get_rect(midbottom = (80, 300))
-
+player_grav = 0
 
 #We need to create a loop that we use infinitely to open the screen window
-while True:
+while True: 
     for event in pygame.event.get():
         # this condtion is about if you press the exit button of the display window it will end the display/game
         if event.type == pygame.QUIT:
@@ -43,12 +44,23 @@ while True:
         #we use sys module to exit the loop so it never encounters an error about initializing the screen.
             exit()
         #we use this event loop to know if the mouse collides on the player rectangle.
-        #if event.type == pygame.MOUSEMOTION:
-            #if player_rectangle.collidepoint(event.pos): print('collision')
-
+        if event.type == pygame.MOUSEBUTTONDOWN:
+            if player_rectangle.collidepoint(event.pos):
+                player_grav = -30
+            
+        #this event loop us to get key inputs using the combined pygame.key() and eventloop method   
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_SPACE:
+                player_grav = -30
+             
+            
     #we use .blit() to call out the created that we made named(test_surface) (block image transfer)       
     screen.blit(sky_surf,(0,0) )
     screen.blit(ground_surf,(0,300))
+    #pygame.draw() use to create/draw any shapes on any part of the screen using this attributes (name of the screen, color, what surface)
+    pygame.draw.rect(screen, '#f1cbff', score_rect)
+    pygame.draw.rect(screen, '#f1cbff', score_rect,20)
+
     screen.blit(score_surf, (score_rect))
 
     #we use .x() so the coordinates of the snail turns left reducing the value by 3 so it can reach the default value of 0
@@ -59,18 +71,12 @@ while True:
 
     #we increment the value of the X position of the snail to move it on the left side of the display
     screen.blit(snail_surf, snail_rect)
+
+    #player
+    #this part also serves as the gravity physics of the player!
+    player_grav += 1
+    player_rectangle.y += player_grav 
     screen.blit(Player_surf, player_rectangle)
-
-    #collision is when the character hits the enemy  it will be game over and we use conditional statements to setup the damage to the character.
-    #if (player_rectangle.colliderect(snail_rect)):
-        #print('collision')
-
-    #pygame.mouse.get_pos() use to know the position of the mouse
-    #pygame.mouse.get_pressed() use to know which button of the mouse is getting pressed.
-
-    #mouse_pos = pygame.mouse.get_pos()
-    #if player_rectangle.collidepoint((mouse_pos)):
-        #print(pygame.mouse.get_pressed())
 
     #inside this loop we draw all of our elements 
     # and update everything that insides the game
@@ -79,7 +85,7 @@ while True:
     clock.tick(60)
     
 
-    #---------- we create basic serface now ---------------------
+
 
 
 
