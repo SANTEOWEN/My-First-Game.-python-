f65eebb0da881da61ed1b6dc39f22a1f51aadfe5
SANTEOWEN <101460774+SANTEOWEN@users.noreply.github.com>
2023-03-04 11:31:04 +0800

    i use the .time.get_ticks() method to record the time and use it as the score system of the game and also addeds some game states which is the game over and restart state next agenda learn more about adding more events like the checkpoint state or make other stages in game

--- game_dev.py
+++ game_dev.py
@@ -1,23 +1,32 @@
 import pygame
 from sys import exit
 
-#init() = it starts the pygame
 
+# This function use to show the time in game which is our score system
+#we created variable valued with 0 so every time the or if the player presses the keyinputs the value of time.ticks will reset to 0, we subtract 0 on the current game time to reset the value of the current time.
+def display_score():
+    current_t = pygame.time.get_ticks() - start_time 
+    score_surf = test_font.render(f'{current_t}',False,(0,0,0))
+    score_rect = score_surf.get_rect(center = (400, 50))
+    screen.blit(score_surf,score_rect)
+
+
+#init() = it starts the pygame
 pygame.init()
 #we use pygame.display.set_mode(()) to create a screen
 #we created a play surface a window use to let the players see whats happening
 screen = pygame.display.set_mode((800,400))
 pygame.display.set_caption('Takbo')
-
 # we use the variable clock to set the default frames per seconds of a the game and we use the pygame.time.Clock() to input what default ticks/frames we want
 clock = pygame.time.Clock()
-
 #we added a font to create basic text on the game. by using pygame.font.Font('filename of the font', size)
 test_font = pygame.font.Font('Fonts/Pixeltype.ttf', 50)
 # We use render() this method use to render any text surfaces and it needs some inputs whic are the ("TEXT", ANTI ALIAZING COUNT U WANT, "COLOR OF THE TEXT")
 
-score_surf = test_font.render('My Game', False, (0, 0, 0))
-score_rect = score_surf.get_rect(center = (400, 50))
+game_active = True
+start_time = 0
+#score_surf = test_font.render('My Game', False, (0, 0, 0))
+#score_rect = score_surf.get_rect(center = (400, 50))
 
 #we create a surface for the game.
 
@@ -35,6 +44,12 @@ Player_surf = pygame.image.load('Graphics/player/player_walk_1.png').convert_alp
 player_rectangle = Player_surf.get_rect(midbottom = (80, 300))
 player_grav = 0
 
+
+
+#---------------------------------------- EVENT/LOOPS SECTION -------------------------------------#
+
+
+
 #We need to create a loop that we use infinitely to open the screen window
 while True: 
     for event in pygame.event.get():
@@ -43,40 +58,54 @@ while True:
             pygame.quit()
         #we use sys module to exit the loop so it never encounters an error about initializing the screen.
             exit()
-        #we use this event loop to know if the mouse collides on the player rectangle.
-        if event.type == pygame.MOUSEBUTTONDOWN:
-            if player_rectangle.collidepoint(event.pos):
-                player_grav = -30
-            
-        #this event loop us to get key inputs using the combined pygame.key() and eventloop method   
-        if event.type == pygame.KEYDOWN:
-            if event.key == pygame.K_SPACE:
-                player_grav = -30
-             
-            
-    #we use .blit() to call out the created that we made named(test_surface) (block image transfer)       
-    screen.blit(sky_surf,(0,0) )
-    screen.blit(ground_surf,(0,300))
-    #pygame.draw() use to create/draw any shapes on any part of the screen using this attributes (name of the screen, color, what surface)
-    pygame.draw.rect(screen, '#f1cbff', score_rect)
-    pygame.draw.rect(screen, '#f1cbff', score_rect,20)
+        if game_active:
+            #we use this event loop to know if the mouse collides on the player rectangle.
+            if event.type == pygame.MOUSEBUTTONDOWN:
+                if player_rectangle.collidepoint(event.pos) and player_rectangle.bottom >= 300:
+                    player_grav = -20
+                
+            #this event loop us to get key inputs using the combined pygame.key() and eventloop method   
+            if event.type == pygame.KEYDOWN:
+                #this combined conditions tells us that the character can only jump if its on the ground surface. 
+                if event.key == pygame.K_SPACE and player_rectangle.bottom >= 300:
+                    player_grav = -20
+        else:
+            #This statement serves as the restart button everytime it hits the space key it will restart the game after the collision of enemy_rectangle.
+            if event.type == pygame.KEYDOWN and event.key == pygame.K_SPACE:
+                game_active = True
+                snail_rect.left = 800
+                start_time = pygame.time.get_ticks()
+    if game_active:       
+        #we use .blit() to call out the created surface that we named(test_surface) (block image transfer)       
+        screen.blit(sky_surf,(0,0) )
+        screen.blit(ground_surf,(0,300))
+        #pygame.draw() use to create/draw any shapes on any part of the screen using this attributes (name of the screen, color, what surface)
+        #pygame.draw.rect(screen, '#f1cbff', score_rect)
+        #pygame.draw.rect(screen, '#f1cbff', score_rect,20)
+        #screen.blit(score_surf, (score_rect))
+        display_score()
 
-    screen.blit(score_surf, (score_rect))
+        #we use .x() so the coordinates of the snail turns left reducing the value by 3 so it can reach the default value of 0
+        snail_rect.x -= 4
+        #in this condition we use the method .right() wiht the value of <= 0 to make the object continue turning left if it hits the value of 800 in coordinate.
+        if snail_rect.right <= 0: snail_rect.left = 800
 
-    #we use .x() so the coordinates of the snail turns left reducing the value by 3 so it can reach the default value of 0
-    snail_rect.x -= 3
+        #we increment the value of the X position of the snail to move it on the left side of the display
+        screen.blit(snail_surf, snail_rect)
 
-    #in this condition we use the method .right() wiht the value of <= 0 to make the object continue turning left if it hits the value of 800 in coordinate.
-    if snail_rect.right <= 0: snail_rect.left = 800
+        #player
+        #this part also serves as the gravity physics of the player!
+        player_grav += 1
+        player_rectangle.y += player_grav 
+        #this conditional statement use to make the character stays on the surface and not fall down because of the gravity variable.
+        if player_rectangle.bottom >= 300: player_rectangle.bottom = 300
+        screen.blit(Player_surf, player_rectangle)
 
-    #we increment the value of the X position of the snail to move it on the left side of the display
-    screen.blit(snail_surf, snail_rect)
-
-    #player
-    #this part also serves as the gravity physics of the player!
-    player_grav += 1
-    player_rectangle.y += player_grav 
-    screen.blit(Player_surf, player_rectangle)
+        # Collisions
+        if snail_rect.colliderect(player_rectangle):
+            game_active = False
+    else:
+        screen.fill('Yellow')
 
     #inside this loop we draw all of our elements 
     # and update everything that insides the game
